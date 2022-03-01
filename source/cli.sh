e93659a28aa0ec4fcc0b1668f18b8765d2b005ed
ivebencrazy <github@bpev.me>
2022-02-28 20:03:26 -0800

    feat: ignore cd error msg, is handled in js

--- source/cli.sh
+++ source/cli.sh
@@ -7,18 +7,19 @@ jd() {
   # Handle cd methods
   if [[ "$1" =~ ^[0-9]{2}-[0-9]{2}$ ]]; then
     # Matches area regex: `dd-dd`. Navigate to area
-    cd $JD_HOME/${1}*/;
+    cd $JD_HOME/${1}*/ 2>/dev/null;
   elif [[ "$1" =~ ^[0-9]{2}$ ]]; then
     # Matches category regex: `dd`. Navigate to category
-    cd $JD_HOME/*/${1}*/;
+    cd $JD_HOME/*/${1}*/ 2>/dev/null;
   # User is specifying "cd". Check for id navigation
   elif [[ "$1" =~ ^[0-9]{2}\.[0-9]{2}$ ]]; then
     # Matches id regex: `dd.dd`. Navigate to id
-    cd $JD_HOME/*/*/${1}*/;
+    cd $JD_HOME/*/*/${1}*/ 2>/dev/null;
   elif [ $# == 0 ]; then
     # No arg for cd. Navigate to root.
-    cd $JD_HOME;
+    cd $JD_HOME 2>/dev/null;
   fi
 
+  # Deno script also handles logging around cd functionality.
   johnny_decimal $*
 }
